715d23dbbdd13ae8bf1673241d745620b60b7e5a
Corina <corina.aj@gmail.com>
2016-01-28 07:31:56 -1000

    add Photo.js which will be the model for individual gallery photos

---
+++ models/Photo.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var Photo = sequelize.define("Photo", {
+    author: DataTypes.STRING,
+    link: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // Photo.belongsTo(models.User);
+      }
+    }
+  });
+  return Photo;
+};
