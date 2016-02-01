db6f5fdb2487ffea8222ddfdd0f745e335c2eecd
BBishop <brad.bishop68@gmail.com>
2016-02-01 09:18:47 -1000

    Creates User table via Models and a user route file

--- models/Photo.js
+++ models/Photo.js
@@ -1,14 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
   var Photo = sequelize.define("Photo", {
-    author: DataTypes.STRING,
-    link: DataTypes.STRING,
-    description: DataTypes.TEXT
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
   }, {
     classMethods: {
       associate: function(models) {
-        // Photo.belongsTo(models.User);
+        Photo.belongsTo(models.Users);
       }
-    }
-  });
+     }
+    });
   return Photo;
 };
