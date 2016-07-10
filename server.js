9ad113c7a859217967d36fff2f9f9bcf35c953ad
Christie R <creindle@hawaii.edu>
2016-07-09 16:50:37 -1000

    POST method is complete :)

--- server.js
+++ server.js
@@ -11,29 +11,50 @@ console.log("Sanity check");
 var express = require('express');
 
 var app = express();
+var querystring = require('querystring');
+
+var dataObjects = "";
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('Returning a list of gallery photos');
 });
 
-app.get('/gallery/4', function (req, res) {
-  res.send('Single gallery 4');
+app.get('/gallery/:id', function (req, res) {
+  //console.log(req.params.id);
+  res.send('Single gallery ' + req.params.id);
 });
 
 app.get('/gallery/new', function (req, res) {
+  //console.log(Object.getOwnPropertyNames(req));
   res.send('Gallery submission form');
 });
 
 app.post('/gallery', function (req, res) {
-  res.send('Creating a gallery with {author}, {url}, {description}');
+
+  req.on('data', function(data) {
+    console.log("Data coming in");
+    dataInput = data.toString();
+    dataObjects = querystring.parse(dataInput);
+    console.log(dataObjects);
+  })
+
+  req.on('end', function() {
+    console.log(dataObjects.author);
+    console.log(dataObjects.url);
+    console.log(dataObjects.description);
+    res.send('Creating a gallery with ' + dataObjects.author + ', ' + dataObjects.url + ', ' + dataObjects.description);
+  })
+
 });
 
-app.put('/gallery/4', function (req, res) {
-  res.send('Updating gallery with {new_author}, {new_url}, {new_description}');
+app.put('/gallery/:id', function (req, res) {
+  res.send('Updating gallery ' + req.params.id + ' with {new_author}, {new_url}, {new_description}');
 });
 
-app.delete('/gallery/4', function (req, res) {
-  res.send('Deleting gallery 4');
+app.delete('/gallery/:id', function (req, res) {
+  res.send('Deleting gallery ' + req.params.id);
 });
 
 var server = app.listen(3000, function () {
