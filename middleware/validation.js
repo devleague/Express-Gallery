7072a37b98585f7ae244ff9f99a51c06e35d573e
Casey Oeda <coeda@live.com>
2016-10-18 23:03:18 -1000

    figured out validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,36 +1,16 @@
   let validate = (req, res, next) => {
-    console.log(req.body.title);
-    if (req.body.title !== "") {
-        if (!(/^[{}<>;]/.test(req.body.title))) {
+    if (/[{}<>;]/g.test(req.body.title) ||
+      /[{}<>;]/g.test(req.body.author) ||
+      /[{}<>;]/g.test(req.body.description) ||
+      /[<>{}[\])(*&^%$@!]/g.test(req.body.hashtags)
+      ){
         res.status(400)
           .json({
-            error: 'Title contained {}, <> or ;'
+            error: 'Invalid Input'
           });
-        }
-    } else if (req.body.author !== "") {
-      if(/^[{}<>\;]+$/.test(req.body.author)) {
-        res.status(400)
-          .json({
-            error: 'Author contained {}, <> or ;'
-          });
-        }
-    } else if (req.body.description !== "") {
-      if(/^[{}<>\;]+$/.test(req.body.description)) {
-        res.status(400)
-        .json({
-          error: 'Description contained {}, <> or ;'
-        });
-      }
-    } else if ((/[a-zA-Z #]/.test(req.body.hashtags))) {
-        res.status(400)
-        .json({
-          error: 'hashtags contained {}, <> or ;'
-        });
     } else {
       next();
     }
-    console.log('hello');
-    next();
   };
 
 
