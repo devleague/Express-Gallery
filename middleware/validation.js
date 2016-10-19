880f2401383e6a1fd7f402e131c0b2c5ed06e878
Casey Oeda <coeda@live.com>
2016-10-18 19:58:04 -1000

    worked on validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,3 +1,37 @@
-module.exports = () => {
-  console.log('test');
-};
+  let validate = (req, res, next) => {
+    console.log(req.body.title);
+    if (req.body.title !== "") {
+        if (!(/^[{}<>;]/.test(req.body.title))) {
+        res.status(400)
+          .json({
+            error: 'Title contained {}, <> or ;'
+          });
+        }
+    } else if (req.body.author !== "") {
+      if(/^[{}<>\;]+$/.test(req.body.author)) {
+        res.status(400)
+          .json({
+            error: 'Author contained {}, <> or ;'
+          });
+        }
+    } else if (req.body.description !== "") {
+      if(/^[{}<>\;]+$/.test(req.body.description)) {
+        res.status(400)
+        .json({
+          error: 'Description contained {}, <> or ;'
+        });
+      }
+    } else if ((/[a-zA-Z #]/.test(req.body.hashtags))) {
+        res.status(400)
+        .json({
+          error: 'hashtags contained {}, <> or ;'
+        });
+    } else {
+      next();
+    }
+    console.log('hello');
+    next();
+  };
+
+
+  module.exports = validate;
