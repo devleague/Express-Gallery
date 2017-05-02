a80bc1974fb933605610819ceaca297d353d9ff6
Dru808 <andrew.tram@yahoo.com>
2017-05-01 18:50:18 -1000

    reoutes all done

--- DB/photos.js
+++ DB/photos.js
@@ -1,3 +1,31 @@
 /*jshint esversion: 6*/
 const express = require('express');
 const router = express.Router();
+
+const addPhoto = (item) => {
+  console.log('add photo');
+};
+
+const editPhoto = (editInfo, editId) => {
+
+};
+
+const removeProduct = (removeInfo) => {
+
+};
+
+const getAllPhotos = () => {
+
+};
+
+const getPhotoById = (photoId) => {
+
+};
+
+module.exports = {
+  addPhoto,
+  editPhoto,
+  removePhoto,
+  getAllPhotos,
+  getPhotoById
+};
--- routes/photos.js
+++ routes/photos.js
@@ -8,7 +8,31 @@ const bodyParser = require('body-parser');
 
 galleryRouter.route('/')
   .post((req, res) => {
+    addPhotos()
     res.send('route works');
+  });
+
+galleryRouter.route('/:id')
+
+  .get((req, res) => {
+    if(req.params.id === 'new') {
+      res.render('new');
+      return;
+    }
+    res.send('get works');
+  })
+
+  .put((req, res) => {
+    res.send('put works');
+  })
+
+  .delete((req, res) => {
+    res.send('delete works');
+  });
+
+galleryRouter.route('/:id/edit')
+  .get((req, res) => {
+    res.send('get edit works');
   });
 
 
